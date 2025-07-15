de9dd5e9122d25945340003bf5d94585af31110a
11sdz <danielzinn641@gmail.com>
2025-07-15 16:30:35 +0300

    patch member route

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -1,43 +1,51 @@
-const prisma = require("../prismaClient")
+const prisma = require("../prismaClient");
 
 const createMemberController = async (data) => {
-    const {
-        full_name,
-        email,
-        profile_img
-    } = data;    
-    
+    const { full_name, email, profile_img } = data;
+
     const newMember = await prisma.communityMember.create({
         data: {
             full_name,
             email,
-            profile_img
-        }
-    })
+            profile_img,
+        },
+    });
 
-    return newMember
-}
+    return newMember;
+};
 
 const getMembersController = async () => {
     const members = await prisma.communityMember.findMany({
         include: {
             job_history: {
-              select: {
-                role: true,
-                company_name: true,
-                start_date: true,
-                end_date: true,
-              }
+                select: {
+                    role: true,
+                    company_name: true,
+                    start_date: true,
+                    end_date: true,
+                },
             },
             groups: {
-              select: {
-                community_name: true
-              }
-            }
-          }
+                select: {
+                    community_name: true,
+                },
+            },
+        },
     });
     return members;
-  }
-  
+};
 
-module.exports = {createMemberController, getMembersController}
+const updateMemberController = async ({ where, data }) => {
+    try {
+        const updatedMember = await prisma.communityMember.update({
+            where,
+            data,
+        });
+        return updatedMember;
+    } catch (error) {
+        console.error("Error in updateMemberController:", error);
+        throw error;
+    }
+};
+
+module.exports = { createMemberController, getMembersController, updateMemberController };
--- src/routes/memberRoutes.js
+++ src/routes/memberRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createMember, getAllMembers } = require("../services/memberService")
+const { createMember, getAllMembers , updateMember } = require("../services/memberService")
 
 
 const router = express.Router();
 
 router.post('/members', createMember);
 router.get('/members', getAllMembers);
+router.patch('/members/:id', updateMember)
 
 module.exports = router;
