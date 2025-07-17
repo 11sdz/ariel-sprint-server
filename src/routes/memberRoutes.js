d7a3b742945d7b2f74ff2153c66409761dcaaac2
shaked40 <shaked@getuna.ai>
2025-07-17 13:41:41 +0300

    suggestions with ai

--- src/routes/memberRoutes.js
+++ src/routes/memberRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getMemberById ,createMember, getAllMembers , updateMember, getSuggestedGroups } = require("../services/memberService")
+const {getMemberById ,createMember, getAllMembers , updateMember, getSuggestedGroups, updateMemberGroups } = require("../services/memberService")
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/members', createMember);
 router.get('/members', getAllMembers);
 router.get('/members/:id', getMemberById);
 router.patch('/members/:id', updateMember);
+router.patch('/members/:id/groups', updateMemberGroups);
 router.get("/members/:memberId/suggestedGroups", getSuggestedGroups);
 
 
